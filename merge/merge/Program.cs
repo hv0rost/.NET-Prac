729f5e6d5f95ca0fc42b58076b2ab7b052480dcd
326 <matyash.danil@mail.ru>
2020-02-04 15:15:26 +0300

    h\w merge

---
+++ merge/merge/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace merge
+{
+    class Program
+    {
+        public static void Sort(object arr)
+        {
+            
+            Array.Sort((int[])arr, 0, ((int[])arr).Length);
+        }
+        static void Main(string[] args)
+        {
+            int[] arr = new int[60];
+            Random rnd = new Random();
+
+            for (int i = 0; i < arr.Length; i++)
+                arr[i] = rnd.Next(0, arr.Length);
+
+            for (int i = 0; i < arr.Length; i++)
+                Console.Write($"{arr[i]} ");
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+            int n = 5;
+            Thread[] sorting = new Thread[n];
+            for(int k = 0; k < n; k++)
+            {
+                sorting[k] = new Thread(new ParameterizedThreadStart(Sort));
+                sorting[k].Start(arr);
+            }
+
+            for (int k = 0; k < n; k++)
+                sorting[k].Join();
+
+            for (int i = 0; i < arr.Length; i++)
+                Console.Write($"{arr[i]} ");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ merge/merge/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("merge")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("merge")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("9ac1ba01-0d3a-477e-b740-6652bf82f568")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номер сборки и номер редакции по умолчанию.
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
