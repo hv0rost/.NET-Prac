617bbaae78d2b9ab2605ce4d9207b102c7ff7ea5
Daniil Matyash <matyash.danil@mail.ru>
2020-02-05 22:24:04 +0300

    Сделал дз по потокам

--- merge/merge/Program.cs
+++ merge/merge/Program.cs
@@ -15,7 +15,10 @@ namespace merge
         }
         static void Main(string[] args)
         {
-            int[] arr = new int[60];
+            Console.Write("Enter count of elements: ");
+            int count = int.Parse(Console.ReadLine());
+
+            int[] arr = new int[count];
             Random rnd = new Random();
 
             for (int i = 0; i < arr.Length; i++)
@@ -29,14 +32,33 @@ namespace merge
 
             int n = 5;
             Thread[] sorting = new Thread[n];
+
             for(int k = 0; k < n; k++)
             {
                 sorting[k] = new Thread(new ParameterizedThreadStart(Sort));
                 sorting[k].Start(arr);
+
             }
 
             for (int k = 0; k < n; k++)
                 sorting[k].Join();
+
+            for (int k = 0; k < n; k++)
+            {
+                int nomerPotoka = k;
+                int chunk = count / n;
+                int start = nomerPotoka * chunk;
+
+                Console.WriteLine(nomerPotoka.ToString() + "-й поток отсортировал: ");
+
+                for (int j = start; j < start + chunk; j++)
+                {
+                    Console.Write(arr[j] + " ");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("\nComleted Array:");
 
             for (int i = 0; i < arr.Length; i++)
                 Console.Write($"{arr[i]} ");
