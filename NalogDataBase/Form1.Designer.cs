94b11e240ff06098d5118717bf00fae29c098bd9
Daniil Matyash <matyash.danil@mail.ru>
2020-05-02 18:31:27 +0300

    Practice

---
+++ NalogDataBase/Form1.Designer.cs
@@ -0,0 +1,205 @@
+﻿namespace NalogDataBase
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.BottomToolStripPanel = new System.Windows.Forms.ToolStripPanel();
+            this.TopToolStripPanel = new System.Windows.Forms.ToolStripPanel();
+            this.RightToolStripPanel = new System.Windows.Forms.ToolStripPanel();
+            this.LeftToolStripPanel = new System.Windows.Forms.ToolStripPanel();
+            this.ContentPanel = new System.Windows.Forms.ToolStripContentPanel();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.updateDataGridViewToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exportToFastReportsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.All_Fields = new System.Windows.Forms.TabPage();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Fields = new System.Windows.Forms.TabControl();
+            this.report1 = new FastReport.Report();
+            this.database1DataSet1 = new NalogDataBase.Database1DataSet();
+            this.menuStrip1.SuspendLayout();
+            this.All_Fields.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.Fields.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.report1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.database1DataSet1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // BottomToolStripPanel
+            // 
+            this.BottomToolStripPanel.Location = new System.Drawing.Point(0, 0);
+            this.BottomToolStripPanel.Name = "BottomToolStripPanel";
+            this.BottomToolStripPanel.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            this.BottomToolStripPanel.RowMargin = new System.Windows.Forms.Padding(4, 0, 0, 0);
+            this.BottomToolStripPanel.Size = new System.Drawing.Size(0, 0);
+            // 
+            // TopToolStripPanel
+            // 
+            this.TopToolStripPanel.Location = new System.Drawing.Point(0, 0);
+            this.TopToolStripPanel.Name = "TopToolStripPanel";
+            this.TopToolStripPanel.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            this.TopToolStripPanel.RowMargin = new System.Windows.Forms.Padding(4, 0, 0, 0);
+            this.TopToolStripPanel.Size = new System.Drawing.Size(0, 0);
+            // 
+            // RightToolStripPanel
+            // 
+            this.RightToolStripPanel.Location = new System.Drawing.Point(0, 0);
+            this.RightToolStripPanel.Name = "RightToolStripPanel";
+            this.RightToolStripPanel.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            this.RightToolStripPanel.RowMargin = new System.Windows.Forms.Padding(4, 0, 0, 0);
+            this.RightToolStripPanel.Size = new System.Drawing.Size(0, 0);
+            // 
+            // LeftToolStripPanel
+            // 
+            this.LeftToolStripPanel.Location = new System.Drawing.Point(0, 0);
+            this.LeftToolStripPanel.Name = "LeftToolStripPanel";
+            this.LeftToolStripPanel.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            this.LeftToolStripPanel.RowMargin = new System.Windows.Forms.Padding(4, 0, 0, 0);
+            this.LeftToolStripPanel.Size = new System.Drawing.Size(0, 0);
+            // 
+            // ContentPanel
+            // 
+            this.ContentPanel.Size = new System.Drawing.Size(150, 150);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.updateDataGridViewToolStripMenuItem,
+            this.exportToFastReportsToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1042, 28);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // updateDataGridViewToolStripMenuItem
+            // 
+            this.updateDataGridViewToolStripMenuItem.Name = "updateDataGridViewToolStripMenuItem";
+            this.updateDataGridViewToolStripMenuItem.Size = new System.Drawing.Size(168, 24);
+            this.updateDataGridViewToolStripMenuItem.Text = "Update DataGridView";
+            this.updateDataGridViewToolStripMenuItem.Click += new System.EventHandler(this.updateDataGridViewToolStripMenuItem_Click);
+            // 
+            // exportToFastReportsToolStripMenuItem
+            // 
+            this.exportToFastReportsToolStripMenuItem.Name = "exportToFastReportsToolStripMenuItem";
+            this.exportToFastReportsToolStripMenuItem.Size = new System.Drawing.Size(164, 24);
+            this.exportToFastReportsToolStripMenuItem.Text = "Export to FastReports";
+            this.exportToFastReportsToolStripMenuItem.Click += new System.EventHandler(this.exportToFastReportsToolStripMenuItem_Click);
+            // 
+            // All_Fields
+            // 
+            this.All_Fields.BackColor = System.Drawing.SystemColors.Window;
+            this.All_Fields.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.All_Fields.Controls.Add(this.dataGridView1);
+            this.All_Fields.Location = new System.Drawing.Point(4, 25);
+            this.All_Fields.Name = "All_Fields";
+            this.All_Fields.Padding = new System.Windows.Forms.Padding(3);
+            this.All_Fields.Size = new System.Drawing.Size(1034, 416);
+            this.All_Fields.TabIndex = 0;
+            this.All_Fields.Text = "All fields";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ButtonFace;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(3, 3);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(1024, 406);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            this.dataGridView1.CellValueChanged += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellValueChanged);
+            this.dataGridView1.EditingControlShowing += new System.Windows.Forms.DataGridViewEditingControlShowingEventHandler(this.dataGridView1_EditingControlShowing);
+            this.dataGridView1.UserAddedRow += new System.Windows.Forms.DataGridViewRowEventHandler(this.dataGridView1_UserAddedRow);
+            // 
+            // Fields
+            // 
+            this.Fields.Controls.Add(this.All_Fields);
+            this.Fields.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Fields.Location = new System.Drawing.Point(0, 28);
+            this.Fields.Name = "Fields";
+            this.Fields.SelectedIndex = 0;
+            this.Fields.Size = new System.Drawing.Size(1042, 445);
+            this.Fields.TabIndex = 2;
+            // 
+            // report1
+            // 
+            this.report1.NeedRefresh = false;
+            this.report1.ReportResourceString = resources.GetString("report1.ReportResourceString");
+            this.report1.Tag = null;
+            this.report1.RegisterData(this.database1DataSet1, "database1DataSet1");
+            // 
+            // database1DataSet1
+            // 
+            this.database1DataSet1.DataSetName = "Database1DataSet";
+            this.database1DataSet1.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1042, 473);
+            this.Controls.Add(this.Fields);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "NalogDataBase";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.All_Fields.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.Fields.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.report1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.database1DataSet1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ToolStripPanel BottomToolStripPanel;
+        private System.Windows.Forms.ToolStripPanel TopToolStripPanel;
+        private System.Windows.Forms.ToolStripPanel RightToolStripPanel;
+        private System.Windows.Forms.ToolStripPanel LeftToolStripPanel;
+        private System.Windows.Forms.ToolStripContentPanel ContentPanel;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem updateDataGridViewToolStripMenuItem;
+        private System.Windows.Forms.TabPage All_Fields;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TabControl Fields;
+        private System.Windows.Forms.ToolStripMenuItem exportToFastReportsToolStripMenuItem;
+        private FastReport.Report report1;
+        private Database1DataSet database1DataSet1;
+    }
+}
+
---
+++ NalogDataBase/Form1.cs
@@ -0,0 +1,296 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NalogDataBase
+{
+    public partial class Form1 : Form
+    {
+        private SqlConnection sqlConnection = null;
+        private SqlDataAdapter dataAdapter = null;
+        private DataTable DataTable = null;
+        private SqlCommandBuilder sqlBuilder = null;
+        private DataSet dataSet = null;
+
+        private bool flag = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+     
+        void LoadDataGridView()
+        {
+            try
+            {
+                dataAdapter = new SqlDataAdapter("SELECT * , 'Delete' AS [Control Cell] FROM Nalog", sqlConnection);
+                sqlBuilder = new SqlCommandBuilder(dataAdapter);
+
+                sqlBuilder.GetDeleteCommand();
+                sqlBuilder.GetInsertCommand();
+                sqlBuilder.GetUpdateCommand();
+
+                dataSet = new DataSet();
+
+                dataAdapter.Fill(dataSet, "Nalog");
+                dataGridView1.DataSource = dataSet.Tables["Nalog"];
+
+                for(int i = 0; i < dataGridView1.Rows.Count; i++)
+                {
+                    DataGridViewLinkCell linkCell = new DataGridViewLinkCell();
+                    dataGridView1[13, i] = linkCell;
+                }
+
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        void UpdateDataGridView()
+        {
+            try
+            {
+                dataSet.Tables["Nalog"].Clear();
+
+                dataAdapter.Fill(dataSet, "Nalog");
+                dataGridView1.DataSource = dataSet.Tables["Nalog"];
+
+                for (int i = 0; i < dataGridView1.Rows.Count; i++)
+                {
+                    DataGridViewLinkCell linkCell = new DataGridViewLinkCell();
+                    dataGridView1[13, i] = linkCell;
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            sqlConnection = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\USER\.NET-Prac\NalogDataBase\Database1.mdf;Integrated Security=True");
+            sqlConnection.Open();
+            LoadDataGridView();
+        }
+     
+        private void updateDataGridViewToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            UpdateDataGridView();
+            MessageBox.Show("DataGridView has updated", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            try
+            {
+                if(e.ColumnIndex == 13)
+                {
+                    string task = dataGridView1.Rows[e.RowIndex].Cells[13].Value.ToString();
+
+
+                    if(task == "Delete")
+                    {
+                        if (MessageBox.Show("Do you want to delete this row?", "Deletion",
+                            MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                        {
+                            int rowIndex = e.RowIndex;
+                            dataGridView1.Rows.RemoveAt(rowIndex);
+                            dataSet.Tables["Nalog"].Rows[rowIndex].Delete();
+                            dataAdapter.Update(dataSet, "Nalog");
+                        }
+                    }
+                    else if (task == "Insert")
+                    {
+                        int rowIndex = dataGridView1.Rows.Count - 2;
+                        DataRow row = dataSet.Tables["Nalog"].NewRow();
+                        DataRow row1 = dataSet.Tables["Nalog"].NewRow();
+
+                        row["count"] = dataGridView1.Rows[rowIndex].Cells["count"].Value;
+                        row["price_for_one"] = dataGridView1.Rows[rowIndex].Cells["price_for_one"].Value;
+                        row["cost_proiz"] = dataGridView1.Rows[rowIndex].Cells["cost_proiz"].Value;
+                        row["other_cost"] = dataGridView1.Rows[rowIndex].Cells["other_cost"].Value;
+                        row["cost_paper"] = dataGridView1.Rows[rowIndex].Cells["cost_paper"].Value;
+                        row["fines"] = dataGridView1.Rows[rowIndex].Cells["fines"].Value;
+                        row["cost_dessolution"] = dataGridView1.Rows[rowIndex].Cells["cost_dessolution"].Value;
+
+                        int income, costs, the_tax_base;
+
+                        row["income"] = Convert.ToInt32(dataGridView1.Rows[rowIndex].Cells["count"].Value) *
+                            Convert.ToInt32(dataGridView1.Rows[rowIndex].Cells["price_for_one"].Value) +
+                            Convert.ToInt32(dataGridView1.Rows[rowIndex].Cells["fines"].Value);
+                        income = Convert.ToInt32(dataGridView1.Rows[rowIndex].Cells["count"].Value) *
+                            Convert.ToInt32(dataGridView1.Rows[rowIndex].Cells["price_for_one"].Value) +
+                            Convert.ToInt32(dataGridView1.Rows[rowIndex].Cells["fines"].Value);
+
+                        row["costs"] = Convert.ToInt32(dataGridView1.Rows[rowIndex].Cells["cost_proiz"].Value) +
+                            Convert.ToInt32(dataGridView1.Rows[rowIndex].Cells["other_cost"].Value) +
+                            Convert.ToInt32(dataGridView1.Rows[rowIndex].Cells["cost_paper"].Value) +
+                            Convert.ToInt32(dataGridView1.Rows[rowIndex].Cells["cost_dessolution"].Value);
+                        costs = Convert.ToInt32(dataGridView1.Rows[rowIndex].Cells["cost_proiz"].Value) +
+                            Convert.ToInt32(dataGridView1.Rows[rowIndex].Cells["other_cost"].Value) +
+                            Convert.ToInt32(dataGridView1.Rows[rowIndex].Cells["cost_paper"].Value) +
+                            Convert.ToInt32(dataGridView1.Rows[rowIndex].Cells["cost_dessolution"].Value);
+
+                        the_tax_base = income - costs;
+                        row["the_tax_base"] = the_tax_base;
+                        
+
+                        row["simplified_tax_system"] = the_tax_base * 0.1;
+
+                        row["simplified_tax_system_min"] = income * 0.01;
+
+
+                        dataSet.Tables["Nalog"].Rows.Add(row);
+                        dataSet.Tables["Nalog"].Rows.RemoveAt(dataSet.Tables["Nalog"].Rows.Count - 1);
+                        dataGridView1.Rows.RemoveAt(dataGridView1.Rows.Count - 2);
+                        dataGridView1.Rows[e.RowIndex].Cells[13].Value = "Delete";
+
+                        dataAdapter.Update(dataSet, "Nalog");
+
+                        flag = false;
+                    }
+                    else if (task == "Change")
+                    {
+                        int r = e.RowIndex;
+
+                        dataSet.Tables["Nalog"].Rows[r]["count"] = dataGridView1.Rows[r].Cells["count"].Value;
+                        dataSet.Tables["Nalog"].Rows[r]["price_for_one"] = dataGridView1.Rows[r].Cells["price_for_one"].Value;
+                        dataSet.Tables["Nalog"].Rows[r]["cost_proiz"] = dataGridView1.Rows[r].Cells["cost_proiz"].Value;
+                        dataSet.Tables["Nalog"].Rows[r]["other_cost"] = dataGridView1.Rows[r].Cells["other_cost"].Value;
+                        dataSet.Tables["Nalog"].Rows[r]["cost_paper"] = dataGridView1.Rows[r].Cells["cost_paper"].Value;
+                        dataSet.Tables["Nalog"].Rows[r]["fines"] = dataGridView1.Rows[r].Cells["fines"].Value;
+                        dataSet.Tables["Nalog"].Rows[r]["cost_dessolution"] = dataGridView1.Rows[r].Cells["cost_dessolution"].Value;
+
+                        int temp;
+                        double  temp_float;
+
+                        temp = Convert.ToInt32(dataGridView1.Rows[r].Cells["count"].Value)
+                        * Convert.ToInt32(dataGridView1.Rows[r].Cells["price_for_one"].Value)
+                        + Convert.ToInt32(dataGridView1.Rows[r].Cells["fines"].Value);
+
+                        dataSet.Tables["Nalog"].Rows[r]["income"] = temp;
+
+                        temp = Convert.ToInt32(dataGridView1.Rows[r].Cells["cost_proiz"].Value)
+                        + Convert.ToInt32(dataGridView1.Rows[r].Cells["other_cost"].Value)
+                        + Convert.ToInt32(dataGridView1.Rows[r].Cells["cost_paper"].Value)
+                        + Convert.ToInt32(dataGridView1.Rows[r].Cells["cost_dessolution"].Value);
+
+                        dataSet.Tables["Nalog"].Rows[r]["costs"] = temp;
+
+                        temp = Convert.ToInt32(dataGridView1.Rows[r].Cells["income"].Value)
+                        - Convert.ToInt32(dataGridView1.Rows[r].Cells["costs"].Value);
+
+                        dataSet.Tables["Nalog"].Rows[r]["the_tax_base"] = temp;
+
+                        temp_float = Convert.ToDouble(dataSet.Tables["Nalog"].Rows[r]["the_tax_base"]) * 0.1;
+                        dataSet.Tables["Nalog"].Rows[r]["simplified_tax_system"] = temp_float;
+
+                        temp_float = Convert.ToDouble(dataSet.Tables["Nalog"].Rows[r]["income"]) * 0.01;
+                        dataSet.Tables["Nalog"].Rows[r]["simplified_tax_system_min"] = temp_float;
+
+                        dataAdapter.Update(dataSet, "Nalog");
+
+                        dataGridView1.Rows[e.RowIndex].Cells[13].Value = "Delete";
+                    }
+                    UpdateDataGridView();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void dataGridView1_UserAddedRow(object sender, DataGridViewRowEventArgs e)
+        {
+            try
+            {
+                if (flag == false)
+                {
+                    flag = true;
+
+                    int lastRow = dataGridView1.Rows.Count - 2;
+                    DataGridViewRow row = dataGridView1.Rows[lastRow];
+                    DataGridViewLinkCell linkCell = new DataGridViewLinkCell();
+                    dataGridView1[13, lastRow] = linkCell;
+                    row.Cells["Control Cell"].Value = "Insert";
+                }
+
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void dataGridView1_CellValueChanged(object sender, DataGridViewCellEventArgs e)
+        {
+            try
+            {
+                if (flag == false)
+                {
+                    int rowIndex = dataGridView1.SelectedCells[0].RowIndex;
+                    DataGridViewRow editRow = dataGridView1.Rows[rowIndex];
+
+                    DataGridViewLinkCell linkCell = new DataGridViewLinkCell();
+                    dataGridView1[13, rowIndex] = linkCell;
+                    editRow.Cells["Control Cell"].Value = "Change";
+
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void dataGridView1_EditingControlShowing(object sender, DataGridViewEditingControlShowingEventArgs e)
+        {
+            e.Control.KeyPress -= new KeyPressEventHandler(KeyPressed);
+
+            if (dataGridView1.CurrentCell.ColumnIndex < 8)
+            {
+                TextBox textBox = e.Control as TextBox;
+
+                if (textBox != null)
+                {
+                    textBox.KeyPress += new KeyPressEventHandler(KeyPressed);
+                }
+            }
+            else
+            {
+                TextBox textBox = e.Control as TextBox;
+                textBox.KeyPress += new KeyPressEventHandler(KeyCanNotBePressed);
+                MessageBox.Show("You can`t fill this cell by your", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void KeyPressed(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+                e.Handled = true;
+        }
+
+        private void KeyCanNotBePressed(object sender, KeyPressEventArgs e)
+        {
+                e.Handled = true;
+        }
+
+        private void exportToFastReportsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            report1.Show();
+        }
+
+    }
+}
